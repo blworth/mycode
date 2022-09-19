762861b3b398c81fb56fb728483f07445b67a50c
Mona Lisa <blworth@users.noreply.github.com>
2022-09-19 22:31:35 +0000

    Monday challenge hw

---
+++ challenge.py
@@ -0,0 +1,16 @@
+user_input_name = input("Please enter your name.")
+name = str.capitalize(user_input_name.strip())
+
+user_input_day = input("Please enter the current day of the week.")
+day = str.capitalize(user_input_day.strip())
+
+print(f"Hello, {name}! Happy {day}!")
+
+
+
+
+name = str.capitalize(input("Please enter your name.").strip())
+
+day = str.capitalize(input("Please enter the current day of the week.").strip())
+
+print(f"Hello, {name}! Happy {day}!")
