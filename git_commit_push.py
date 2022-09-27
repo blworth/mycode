f037e916478a1df0fab729197390313a70b05dde
Mona Lisa <blworth@users.noreply.github.com>
2022-09-27 23:10:00 +0000

    test

--- git_commit_push.py
+++ git_commit_push.py
@@ -1,7 +1,8 @@
 import os
-os.system("cd ~/mycode")
-os.system("git commit")
-os.system(f"git add *")
+os.system("cd ~/mycode && git commit")
+os.system("cd ~/mycode && git add *")
+#os.system("git commit")
+#os.system(f"git add *")
 commit_message = input("What would you like you input message to be?")
-os.system(f"git commit -m '{commit_message}'")
-os.system("git push origin HEAD")
+os.system(f"cd ~/mycode && git commit -m '{commit_message}' && git push origin HEAD")
+# os.system("git push origin HEAD")
