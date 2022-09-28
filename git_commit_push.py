fae2f1cbcf8335e81a5ccc15c68236cf8b69e74b
blworth <worthambrandon@gmail.com>
2022-09-28 15:42:48 +0000

    testing final

--- git_commit_push.py
+++ git_commit_push.py
@@ -1,6 +1,18 @@
+#!/usr/bin/env python3
+"""Git Add, Commit, Push to main"""
+
 import os
-os.system("git status") #this will display the files you can commit
-git_add = input("What files would you like to commit? i.e. Choose from above or write * to add all")
-os.system(f"cd ~/mycode && git add {git_add}")
-commit_message = input("What would you like you input message to be?")
-os.system(f"cd ~/mycode && git commit -m '{commit_message}' && git push origin HEAD")
+
+def main():
+    os.system("git status") #this will display the files you can commit
+
+    git_add = input("What files would you like to commit? i.e. Choose from above or write * to add all")
+    
+    os.system(f"cd ~/mycode && git add {git_add}")
+    
+    commit_message = input("What would you like you input message to be?")
+    
+    os.system(f"cd ~/mycode && git commit -m '{commit_message}' && git push origin HEAD")
+
+if __name__ == "__main__":
+    main()
