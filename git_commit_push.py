b3a0c3994be03a72cf946227730f25f5898aec75
blworth <worthambrandon@gmail.com>
2022-09-28 15:34:11 +0000

    test

--- git_commit_push.py
+++ git_commit_push.py
@@ -1,8 +1,4 @@
 import os
-os.system("cd ~/mycode && git commit")
 os.system("cd ~/mycode && git add *")
-#os.system("git commit")
-#os.system(f"git add *")
 commit_message = input("What would you like you input message to be?")
 os.system(f"cd ~/mycode && git commit -m '{commit_message}' && git push origin HEAD")
-# os.system("git push origin HEAD")
