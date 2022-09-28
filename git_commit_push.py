77f3ee2abf28905ff533de5a7ea70df23b7ff058
blworth <worthambrandon@gmail.com>
2022-09-28 15:39:45 +0000

    test1

--- git_commit_push.py
+++ git_commit_push.py
@@ -1,4 +1,6 @@
 import os
-os.system("cd ~/mycode && git add *")
+os.system("git status") #this will display the files you can commit
+git_add = input("What files would you like to commit? i.e. Choose from above or write * to add all")
+os.system(f"cd ~/mycode && git add {git_add}")
 commit_message = input("What would you like you input message to be?")
 os.system(f"cd ~/mycode && git commit -m '{commit_message}' && git push origin HEAD")
